5e1a859e050e16a646de051ab69b05e09d76f492
Pavlo-Hnatenko <gnatenko.pavlo@gmail.com>
2021-03-16 21:03:13 +0200

    commit #1: initialize project

---
+++ src/test/java/com/pavlohnatenko/simplespring/SimplespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavlohnatenko.simplespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
